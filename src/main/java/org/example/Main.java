a9291015bd4dc6eb451b5c23f4e834c2ebb90ef3
Oscar Alfredo Chafloque Tampeck <oscar.chafloque@soytul.com>
2023-09-19 23:36:50 -0500

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,31 +16,16 @@ public class Main {
     private static Faker faker = new Faker();
 
     public static void main(String[] args) {
-        /*List<Student> studentList = new ArrayList<>();
+        List<Student> studentList = new ArrayList<>();
         for (int i = 0; i < 6; i++) {
             studentList.add(new Student(UUID.randomUUID(), faker.name().name(), faker.number().randomDigitNotZero(), (i < 3 ? "Lima" : "Callao")));
-        }*/
-        //System.out.println("groupStudentByLocation = " + groupStudentByLocation(studentList));
-        //System.out.println("groupStudentByRolNumber = " + groupStudentByRolNumber(studentList));
-        //sorted(studentList, "ASC").forEach(student -> System.out.println("student.getName() = " + student.getRolNumber()));
-        //System.out.println("filterByRolNumber = " + filterByRolNumber(studentList, 2));
-        //sorted(studentList, "DES").forEach(student -> System.out.println("student.getName() = " + student.getRolNumber()));
-        List<Integer> nums = Arrays.asList( 4,5,7,3,2,6);
-        int result = 0;
-        result = nums.stream()
-                .filter(integer -> integer%2==0)
-                .map(integer -> integer*2)
-                .reduce(0, (integer, integer2) -> integer + integer2 );
-
-        System.out.println("result = " + result);
-
-        result = (int) nums.stream()
-                .filter(integer -> integer%2==0)
-                .map(integer -> integer*2)
-                .count();
-
-        System.out.println("result = " + result);
-
+        }
+        System.out.println("groupStudentByLocation = " + groupStudentByLocation(studentList));
+        System.out.println("groupStudentByRolNumber = " + groupStudentByRolNumber(studentList));
+        sorted(studentList, "ASC").forEach(student -> System.out.println("student.getName() = " + student.getRolNumber()));
+        System.out.println("filterByRolNumber = " + filterByRolNumber(studentList, 2));
+        sorted(studentList, "DES").forEach(student -> System.out.println("student.getName() = " + student.getRolNumber()));
+        System.out.println("testStreamWithNumber(Arrays.asList( 4,5,7,3,2,6)) = " + testStreamWithNumber(Arrays.asList( 4,5,7,3,2,6)));
     }
 
     //Grouping By Location
@@ -64,5 +49,12 @@ public class Main {
 
     private static List<Student> sorted(List<Student> studentList, String type) {
         return (type.equalsIgnoreCase("ASC") ? studentList.stream().sorted(Comparator.comparingInt(Student::getRolNumber)).collect(Collectors.toList()) : studentList.stream().sorted(Comparator.comparingInt(Student::getRolNumber).reversed()).collect(Collectors.toList()));
+    }
+    
+    private static int testStreamWithNumber(List<Integer> nums){
+        return nums.stream()
+                .filter(integer -> integer%2==0)
+                .map(integer -> integer*2)
+                .reduce(0, (integer, integer2) -> integer + integer2 );
     }
 }
